6dc7d4fac8d36fccd8479dfe2783962cef5e7d94
Isha Rawat <76896205+isharawat@users.noreply.github.com>
2023-01-19 23:59:14 +0530

    initial commit

---
+++ aa.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define dbg(x) cerr<<#x<<" = "<<x<<endl
+
+void solve() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
+ 
+    int tt = 1; cin >> tt;
+    while(tt--) solve();
+    return 0;
+}
